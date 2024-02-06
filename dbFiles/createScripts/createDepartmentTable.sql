48d3ee946f8d17485dc580a3de492272801c60a6
Crush Bate <50185023+batec2@users.noreply.github.com>
2024-02-06 00:30:45 -0700

    table create scripts, reorganized files

---
+++ dbFiles/createScripts/createDepartmentTable.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Departments (
+    DepartmentID INT PRIMARY KEY IDENTITY(1,1),
+    DepartmentName VARCHAR(255) NOT NULL
+);
---
+++ dbFiles/createScripts/createTeachersTable.sql
@@ -0,0 +1,19 @@
+USE [school-registration-db]
+GO
+
+/****** Object:  Table [dbo].[teachers]    Script Date: 2024-02-05 10:09:06 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[teachers](
+	[teacher_id] [int] NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[first_name] [nvarchar](50) NOT NULL,
+	[last_name] [nvarchar](50) NOT NULL,
+	[dept_id] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+GO
+
+
