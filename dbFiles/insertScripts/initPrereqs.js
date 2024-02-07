cdec896a28841e6af46763b32b0b623f3162dec1
Crush Bate <50185023+batec2@users.noreply.github.com>
2024-02-07 00:26:31 -0700

    Script to generate prereqs

--- dbFiles/insertScripts/initPrereqs.js
+++ dbFiles/insertScripts/initPrereqs.js
@@ -1,48 +1,72 @@
-import * as fs from "fs";
-import { parse } from "csv-parse";
 import sql from "mssql";
 import dotenv from "dotenv";
 dotenv.config();
 
-async function connect() {
-  const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    database: process.env.DB_NAME,
-    options: {
-      encrypt: false,
-      trustServerCertificate: true,
-    },
-  };
-  try {
-    await sql.connect(config);
-    return;
-  } catch (err) {
-    console.log(err);
-    return;
-  }
-}
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  options: {
+    encrypt: false,
+    trustServerCertificate: true,
+  },
+};
 
-connect().then(async () => {
-  //   const request = new sql.Request();
+const getCourses = async (level) => {
   const result =
-    await sql.query`select * from courses where course_number >=100 and course_number<200`;
-  console.dir(result);
+    await sql.query`select course_symbol,course_number from courses where course_number >=${level} and course_number<${level + 100}`;
+  return result.recordset
+    .map((value) => [value.course_number, value.course_symbol])
+    .reduce((accumulator, currentValue) => {
+      if (!Object.prototype.hasOwnProperty.call(accumulator, currentValue[1])) {
+        accumulator[currentValue[1]] = [];
+      }
+      accumulator[currentValue[1]].push(currentValue[0]);
+      return accumulator;
+    }, {});
+};
 
-  sql.close();
-  //   const table = new sql.Table("courses");
-  //   table.create = true;
-  //   table.columns.add("course_name", sql.NChar(10), { nullable: false });
-  //   table.columns.add("course_number", sql.Int, { nullable: false });
-  //   table.columns.add("prereq_name", sql.NChar(10), { nullable: false });
-  //   table.columns.add("prereq_number", sql.Int, { nullable: false });
+const addPrereqs = (higherCourse, lowerCourse, table) => {
+  for (const symbol in higherCourse) {
+    higherCourse[symbol].forEach((course) => {
+      if (lowerCourse[symbol] !== undefined) {
+        const prereqs = lowerCourse[symbol].find(
+          (value) => value % 100 === course % 100,
+        );
+        if (prereqs !== undefined) {
+          table.rows.add(symbol, course, symbol, prereqs);
+        }
+      }
+    });
+  }
+};
 
-  //   table.rows.add();
-  //   request.bulk(table, (err, result) => {
-  //     if (err) {
-  //       console.log(err);
-  //     }
-  //     console.log(result);
-  //   });
+sql.connect(config).then(async () => {
+  const table = new sql.Table("prereqs");
+  table.create = true;
+  table.columns.add("course_symbol", sql.NChar(10), { nullable: false });
+  table.columns.add("course_number", sql.Int, { nullable: false });
+  table.columns.add("prereq_symbol", sql.NChar(10), { nullable: false });
+  table.columns.add("prereq_number", sql.Int, { nullable: false });
+
+  // Creates lists of all X00 Level courses
+  const oneHundredLevel = await getCourses(100);
+  const twoHundredLevel = await getCourses(200);
+  const threeHundredLevel = await getCourses(300);
+  const fourHundredLevel = await getCourses(400);
+
+  // Checks if there are any courses with same last two digits in previous X00 level
+  addPrereqs(fourHundredLevel, threeHundredLevel, table);
+  addPrereqs(threeHundredLevel, twoHundredLevel, table);
+  addPrereqs(twoHundredLevel, oneHundredLevel, table);
+
+  const request = new sql.Request();
+  request.bulk(table, (err, result) => {
+    if (err) {
+      console.log("here", err);
+    }
+    console.log(result);
+    sql.close();
+  });
 });
